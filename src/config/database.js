16dc3d51af52d884f7ffe1c2f88fa339f85c17e8
Faris Syafiq Sujana <farissujana1605@gmail.com>
2026-07-20 11:19:46 +0700

    fix: database config

--- src/config/database.js
+++ src/config/database.js
@@ -16,7 +16,7 @@ if (process.env.DB_DIALECT === "sqlite") {
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
-      posrt: process.env.DB_PORT,
+      port: process.env.DB_PORT,
       dialect: process.env.DB_DIALECT,
       logging: isProduction ? false : console.log,
     },
